ba8155ecdd91a57b5069eaf7283ec5ad66a398c9
Dadapeer007 <141058749+Dadapeer007@users.noreply.github.com>
2023-09-11 14:42:42 +0530

    Update 03Subarray with least average.js

--- Assignment-07/03Subarray with least average.js
+++ Assignment-07/03Subarray with least average.js
@@ -1,3 +1,38 @@
+/*Q3. Subarray with least average
+Problem Description
+Given an array of size N, find the subarray of size K with the least average.
+Problem Constraints
+1<=k<=N<=10^5 to 10^5<=A[i]<=10^5
+
+Input Format
+First argument contains an array A of integers of size N.
+Second argument contains integer k.
+
+Output Format
+Return the index of the first element of the subarray of size k that has least average.
+Array indexing starts from 0.
+
+Example Input
+Input1:
+A=[3,7,90,20,10,50,40]
+B=3
+
+Input2:
+A=[3,7,5,20,-10,0,12]
+B=2
+
+Example Output
+Output1:3
+Output2:4
+
+Example Explanation
+Explanation 1:
+Subarray between indexes 3 and 5
+The subarray {20,10,50} has the least average 
+among all subarrays of size 3.
+Explanation 2:
+Subarray between [4,5] has minimum average.*/
+
 function subarrayWithLeastAverage(A,K) 
 {
     if(A.length<K)
