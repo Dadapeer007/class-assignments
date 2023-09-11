2452eb2ee3211968c1a66ffa20ee84c2c60673cd
Dadapeer007 <141058749+Dadapeer007@users.noreply.github.com>
2023-09-11 14:58:57 +0530

    Update 04matrixs multi.js

--- Assignment-09/04matrixs multi.js
+++ Assignment-09/04matrixs multi.js
@@ -1,3 +1,36 @@
+/*Q4. Matrix Multiplication
+Problem Description
+You are given two integer matrices A(having M X N size) and B(having N X P).
+You have to multiply matrix A with B and return the resultant matrix. (i.e. return the matrix AB).
+Problem Constraints
+1 <= M, N, P <= 100 to 100 <= A[i][j], B[i][j] <= 100
+
+Input Format
+The first argument given is the 2-D integer matrix A.
+The second argument given is the 2-D integer matrix B.
+Output Format
+Return a 2D integer matrix denoting AB.
+Example Input
+
+Input 1:
+A = [[1, 2],
+     [3, 4]]
+B = [[5, 6],
+     [7, 8]]
+
+Input 2:
+A = [[1, 1]]
+B = [[2],
+     [3]]
+
+Example Output
+
+Output 1:
+ [[19, 22],
+  [43, 50]]
+Output 2:
+ [[5]]*/
+
 function multiplyMatrices(A,B){
     const M=A.length
     const N=A[0].length 
