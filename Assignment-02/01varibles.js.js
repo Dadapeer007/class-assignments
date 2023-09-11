cc48cb528bd0e742ed5c81b18e1818d1af308457
Dadapeer007 <141058749+Dadapeer007@users.noreply.github.com>
2023-09-11 14:08:57 +0530

    Update 01varibles.js.js

--- Assignment-02/01varibles.js.js
+++ Assignment-02/01varibles.js.js
@@ -1,3 +1,4 @@
+/*[Variables] Declare different types of variables and print the type of each variable?*/
 
 let var1=42;                      
 let var2=3.14                     
@@ -5,8 +6,8 @@ let var3="Hello, World!"
 let var4=[1, 2, 3]              
 let var5={name:"John",age:25}
 
-console.log(typeof var1)
-console.log(typeof var2)
-console.log(typeof var3)
-console.log(typeof var4)
-console.log(typeof var5)
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+console.log(typeof var5);
