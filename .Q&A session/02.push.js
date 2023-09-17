ac6875ee14d0d392eb0d2e9b61049b5801beefab
Dadapeer007 <141058749+Dadapeer007@users.noreply.github.com>
2023-09-18 00:33:02 +0530

    Delete .Q&A session directory

--- .Q&A session/02.push.js
+++
@@ -1,7 +0,0 @@
-let arr=[1,2,3]
-let reverstr=[];
-    for(let i=arr.length-1;i>=0;i--)
-    {
-      reverstr.push(arr[i])
-    }
-  console.log(reverstr);
--- .Q&A session/03.slice.js
+++
@@ -1,9 +0,0 @@
-let str="hello";
-let reversedStr="";
-
-while(str.length>0) {
-  reversedStr = reversedStr+str[str.length-1];
-  str=str.slice(0,str.length-1);
-}
-
-console.log("Reversed string:",reversedStr);
